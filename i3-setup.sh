0ffa6c9d89485e8f89e3b565c94de636c6cf273b
Elliot S <Elliot@sire.dstl.gov.uk>
2020-06-30 10:18:33 +0100

    Copy i3 config.

--- i3-setup.sh
+++ i3-setup.sh
@@ -12,11 +12,12 @@ sudo apt-get install -y compton hsetroot
 # TODO configure these
 
 # Copy in config file
-# TODO
+cp i3-config.txt ~/.config/i3/config
+# TODO check errors
 
 # Fix scaling - make Xresources file
 #ref: https://www.reddit.com/r/i3wm/comments/6kxq83/i3wm_scaling_is_correct_but_programs_are_not_hidpi/
-# TODO make value conditional based on screen size?
+# TODO make value conditional based on screen size? Currently good only for 1080p
 touch ~/.Xresources
 echo "Xft.dpi: 96" > ~/.Xresources
 xrdb ~/.Xresources
