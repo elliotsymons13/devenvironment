9722c6a0db323c1cfd4201d52572f0265e95ca52
Elliot S <elliotsymons13@gmail.com>
2020-12-06 06:44:50 -0800

    Refactor error handling to function.

--- dev-env-setup.sh
+++ dev-env-setup.sh
@@ -39,143 +39,93 @@ while getopts "esh:" thisinput; do
     esac
 done
 
+# params: 
+# 1 = status of previous command
+# 2 = success message
+# 3 = failure message
+function check_status() {
+    # TODO check if num params not expected
+
+    if [ $1 -eq 0 ] 
+    then
+        printf "%s \n\n" "$2" | tee -a $logfile
+    else 
+        printf "=== FAILURE: %s === \n" "$3" | tee -a $logfile
+        if [ "$exitonfail" = true ]; 
+        then
+            echo "Exiting... see log for details" | tee -a $logfile
+            exit 1
+        fi
+    fi
+    if [ "$stepwise" = true ] ; 
+    then
+        read -rsn1 -p"Press any key to continue";echo
+    fi
+}
+
+
 
 # Setup keyboard - UK layout
 #ref: https://askubuntu.com/questions/356763/setting-up-a-uk-keyboard-layout
+echo "Setting keyboard layout to GB..." | tee -a $logfile
 setxkbmap -layout gb
-if [ $? -eq 0 ] 
-then
-    printf "Sucessfully set GB keybaord layout.  \n\n" | tee -a $logfile
-else 
-    printf "=== FAILURE to set GB keybaord layout === \n" | tee -a $logfile
-    if [ "$exitonfail" = true ]; 
-    then
-        echo "Exiting... see log for details" | tee -a $logfile
-        exit 1
-    fi
-fi
-if [ "$stepwise" = true ] ; 
-then
-    read -rsn1 -p"Press any key to continue";echo
-fi
+check_status $? "Sucessfully set GB keyboard layout." "Could not set GB keyboard layout"
+
 
 # Update
 echo "Running apt-get update... (long)" | tee -a $logfile
-sudo apt-get update >> $logfile
-if [ $? -eq 0 ] 
-then
-    printf "atp-get update completed.  \n\n" | tee -a $logfile
-else 
-    printf "=== FAILURE to apt-get update === \n" | tee -a $logfile
-    if [ "$exitonfail" = true ]; 
-    then
-        echo "Exiting... see log for details" | tee -a $logfile
-        exit 1
-    fi
-fi
-if [ "$stepwise" = true ] ; 
-then
-    read -rsn1 -p"Press any key to continue";echo
-fi
-echo "Running apt-get upgrade... (longer)" | tee -a $logfile
-sudo apt-get upgrade -y >> $logfile
-if [ $? -eq 0 ] 
-then
-    printf "atp-get upgrade completed.  \n\n" | tee -a $logfile
-else 
-    printf "=== FAILURE to apt-get upgrade === \n" | tee -a $logfile
-    if [ "$exitonfail" = true ]; 
-    then
-        echo "Exiting... see log for details" | tee -a $logfile
-        exit 1
-    fi
-fi
-if [ "$stepwise" = true ] ; 
-then
-    read -rsn1 -p"Press any key to continue";echo
-fi
+sudo apt-get update >> $logfile 2>&1
+check_status "apt-get update completed." "Could not apt-get update"
 
-# Install packages
+# Upgrade
+echo "Running apt-get upgrade... (longer)" | tee -a $logfile
+sudo apt-get upgrade -y >> $logfile 2>&1
+check_status $? "apt-get upgrade completed." "Could not apt-get upgrade"
+
+
+# Install packages based on contents of packages-for-install.txt
 # File must have LF line endings. 
 echo "Installing packages from list..." | tee -a $logfile
-xargs sudo apt-get install -y < packages-for-install.txt  >> $logfile
-if [ $? -eq 0 ] 
-then
-    printf "ALL package installs completed succesfully.  \n\n" | tee -a $logfile
-else 
-    printf "=== FAILURE to install all packages ===\nCheck logs... \n" | tee -a $logfile
-    if [ "$exitonfail" = true ]; 
-    then
-        echo "Exiting... see log for details" | tee -a $logfile
-        exit 1
-    fi
-fi
-if [ "$stepwise" = true ] ; 
-then
-    read -rsn1 -p"Press any key to continue";echo
-fi
+xargs sudo apt-get install -y < packages-for-install.txt  >> $logfile 2>&1
+check_status $? "Installed ALL packages sucessfully. " "Could not install one or more requested package. See $logfile for details. "
 
 
 # Install chrome
+# Download
 echo "Installing chrome..." | tee -a $logfile
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb >> $logfile
-if [ $? -eq 0 ] 
-then
-    printf "Got download..  \n" | tee -a $logfile
-else 
-    printf "=== FAILURE to download chrome === \n" | tee -a $logfile
-    if [ "$exitonfail" = true ]; 
-    then
-        echo "Exiting... see log for details" | tee -a $logfile
-        exit 1
-    fi
-fi
-sudo apt-get install -y ./google-chrome-stable_current_amd64.deb  >> $logfile
-if [ $? -eq 0 ] 
-then
-    printf "Installed.  \n" | tee -a $logfile
-else 
-    printf "=== FAILURE to install chrome === \n" | tee -a $logfile
-    if [ "$exitonfail" = true ]; 
-    then
-        echo "Exiting... see log for details" | tee -a $logfile
-        exit 1
-    fi
-fi
-if [ "$stepwise" = true ] ; 
-then
-    read -rsn1 -p"Press any key to continue";echo
-fi
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb >> $logfile 2>&1
+check_status $? "Downloaded chrome sucessfully. " "Could not download chrome. "
+
+# Install
+sudo apt-get install -y ./google-chrome-stable_current_amd64.deb  >> $logfile 2>&1
+check_status $? "Installed chrome sucessfully. " "Could not install chrome. "
+
+# Set chrome as default
+sudo update-alternatives --config x-www-browser << EOF
+0
+EOF
+check_status $? "Set chrome as default browser. " "Could not set default browser. "
 
 
 # Install VSCode (needs repo dependency)
 #ref: https://linuxize.com/post/how-to-install-visual-studio-code-on-ubuntu-18-04/
 echo "Installing vscode..." | tee -a $logfile
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -  >> $logfile
-if [ $? -eq 0 ] 
-then
-    printf "Got download..  \n" | tee -a $logfile
-else 
-    printf "=== FAILURE to download chrome === \n" | tee -a $logfile
-    if [ "$exitonfail" = true ]; 
-    then
-        echo "Exiting... see log for details" | tee -a $logfile
-        exit 1
-    fi
-fi
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"  >> $logfile
-sudo apt update  >> $logfile
-sudo apt install -y code  >> $logfile
-if [ "$stepwise" = true ] ; 
-then
-    read -rsn1 -p"Press any key to continue";echo
-fi
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -  >> $logfile 2>&1
+check_status $? "Got vscode step 1/3..." "Could not get vscode"
+
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"  >> $logfile 2>&1
+check_status $? "Got vscode step 2/3..." "Could not get vscode"
+sudo apt update  >> $logfile 2>&1
+check_status $? "Got vscode step 3/3..." "Could not get vscode"
+sudo apt install -y code  >> $logfile 2>&1
+check_status $? "Installed vscode. " "Could not install vscode"
+
 
 # Install VScode extensions
 echo "Installing vscode extensions..." | tee -a $logfile
 cat vscode-extensions-for-install.txt | while read line
 do
-    code --install-extension $line  >> $logfile
+    code --install-extension $line  >> $logfile 2>&1
 done
 echo "Done. " | tee -a $logfile
 if [ "$stepwise" = true ] ; 
@@ -186,22 +136,15 @@ fi
 # Copy in vscode settings
 echo "Copying vscode settings..."
 mkdir -p ~/.config/Code/User/
-cp vscode-settings.json ~/.config/Code/User/settings.json  >> $logfile
-if [ $? -eq 0 ] 
-then
-    printf "Copied.  \n" | tee -a $logfile
-else 
-    printf "=== FAILURE to copy vscode settings === \n" | tee -a $logfile
-    if [ "$exitonfail" = true ]; 
-    then
-        echo "Exiting... see log for details" | tee -a $logfile
-        exit 1
-    fi
-fi
-if [ "$stepwise" = true ] ; 
-then
-    read -rsn1 -p"Press any key to continue";echo
-fi
+check_status $? "Created vscode config directory for user. " "Could not create vscode config dir"
+cp vscode-settings.json ~/.config/Code/User/settings.json  >> $logfile 2>&1
+check_status $? "Copied vscode settings. " "Could not copy vscode settings"
 
 
-echo "SETUP COMPLETE. If not run with -e, check for errors. " | tee -a $logfile
+# Setup Gnome favorites bar
+gsettings set org.gnome.shell favorite-apps "['google-chrome.desktop', 'org.gnome.Nautilus.desktop', 'code.desktop', 'org.gnome.Terminal.desktop']"
+check_status $? "Set favorites bar icons" "Could not set favorites bar icons"
+
+
+
+echo "SETUP COMPLETE. If not run with -e, check output and/or $logfile manually for errors (if you care). " | tee -a $logfile
